266866d6c73cf93578c34450c50ff5020bb5f3a6
ArcosJuan <arcos0juan@gmail.com>
2025-12-29 23:39:31 -0300

    Al ej04 de la p1 no le quedo otra que rendirse

--- p1Haskell/p01e04.hs
+++ p1Haskell/p01e04.hs
@@ -1,5 +1,28 @@
+-- I)
+
 permutaciones :: [a] -> [[a]]
 permutaciones = foldr (\x rec -> concatMap (mezcla x) rec) [[]]
 
 mezcla :: a -> [a] -> [[a]]
 mezcla x xs = foldr (\n rec -> ((take n xs) ++ x : (drop n xs)):rec ) [] [0..(length xs)]
+
+-- II)
+
+partes :: [a] -> [[a]]
+partes = foldr (\x rec ->  rec++(map (x:) rec)) [[]]
+
+-- III)
+
+-- Con foldl
+prefijos :: [a] -> [[a]]
+prefijos = foldl (\rec x -> rec++[(last rec)++[x]]) [[]]
+
+-- Con foldr
+prefijos2 :: [a] -> [[a]]
+prefijos2 xs = foldr (\x rec -> (take x xs):rec) [[]]  [1..(length xs)]
+
+-- IV)
+sublistas :: [a] -> [[a]]
+sublistas xs = foldr (\x rec -> (tail (prefijos (drop x xs))) ++ rec) [[]] [0..(length xs)]
+
+
